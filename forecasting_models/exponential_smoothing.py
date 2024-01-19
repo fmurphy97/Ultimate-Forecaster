2c3f90b20dc9827aa26ffa164e8559aa03fec79e
fmurphy97 <federicoianmurphy@gmail.com>
2024-01-19 18:38:07 -0300

    Add functionality to add days to the df in exp smoothing if the original df does not have them

--- forecasting_models/exponential_smoothing.py
+++ forecasting_models/exponential_smoothing.py
@@ -24,24 +24,32 @@ class ExponentialSmoothing(ForecastModel):
         """Predicts values using the selected dates"""
         # TODO: create method
         fit = list(self.model.fittedvalues)
-        pred = list(self.model.forecast(len(x_test)))
+        values_to_forecast = len(x_test)
+        pred = list(self.model.forecast(values_to_forecast))
 
-        self.df[self.model_name] = fit
+        full_pred = fit + pred
 
-        predictions = pd.DataFrame({
-            self.model_name: pred,
-            self.x_col_name: pd.date_range(self.df[self.x_col_name].max() + pd.Timedelta(days=1),
-                                           periods=len(x_test), freq="D")})
+        # Calculate the start date for the new date range by adding one day to the maximum date
+        start_date = self.df[self.x_col_name].max() + pd.Timedelta(days=1)
+        # Generate the date range
+        new_date_range = pd.date_range(start=start_date, periods=values_to_forecast, freq="D")
+        # Create a new DataFrame with the date range
+        date_range_df = pd.DataFrame({self.x_col_name: new_date_range})
 
-        return predictions
+        # Concatenate the original DataFrame with the new date range DataFrame
+        result_df = pd.concat([self.df[[self.x_col_name]], date_range_df], ignore_index=True)
+
+        result_df[self.model_name] = full_pred
+
+        self.df = pd.merge(self.df, result_df, on=self.x_col_name, how="outer")
+
+        return result_df
 
 
 if __name__ == "__main__":
     df_alb = pd.read_csv("../data/peajes_alberdi_training_preproc.csv")
     df_alb['date'] = pd.to_datetime(df_alb['fecha'], format='%Y-%m-%d')
 
-    # df_no_nan = df_alb[df_alb['fecha'] < '2019-07-01']
-    # df_nan = df_alb[df_alb['fecha'] >= '2019-07-01']
     df_alb = df_alb[df_alb['fecha'] < '2019-07-01']
 
 
@@ -49,4 +57,4 @@ if __name__ == "__main__":
     model_instance = model_class(df=df_alb, x_col_name='date', y_col_name='cantidad_pasos', smoothing_level=0.5)
     model_instance.fit_train()
     y_test = model_instance.predict(df_alb[['date']])
-    model_instance.plot_results(start_date='2016-01-01', end_date='2016-07-01')
+    model_instance.plot_results(start_date='2019-01-01', end_date='2020-07-01')
