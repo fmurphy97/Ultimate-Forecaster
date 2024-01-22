bc1fb85470dbfd1f3d707bad8f67b41727abb336
fmurphy97 <federicoianmurphy@gmail.com>
2024-01-22 12:39:36 -0300

    Add docstrings to forecasting model

--- forecasting_models/forecast_model.py
+++ forecasting_models/forecast_model.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 
 class ForecastModel:
-    def __init__(self, df, x_col_name, y_col_name, external_regressors=None):
+    def __init__(self, df, x_col_name="date", y_col_name="y", external_regressors=None):
         """
         :param df: (pd.Dataframe) Dataset with training time series.
         :param x_col_name: (str, optional) Name of the column that contains the dates. Defaults to "date".
@@ -24,9 +24,20 @@ class ForecastModel:
             self.exogenous_train = None
 
     def fit_train(self):
+        """
+        Fits the forecasting model to the training data.
+        """
         pass
 
     def predict(self, x_test):
+        """
+        Predicts target variable values for the given input dates
+
+        :param x_test: pd.DataFrame
+            Input dates for which predictions are to be made.
+        :return: pd.DataFrame
+            Predicted values for the target variable.
+        """
         pass
 
     def plot_results(self, start_date: str, end_date: str) -> None:
